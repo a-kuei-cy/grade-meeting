009849069064065bc0e73afc63fb31fc4750787d
a-kuei-cy <tea@sces.cy.edu.tw>
2026-09-11 16:20:13 +0800

    更新 config.js via EZPage

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  SCHOOL_NAME: '嘉義市西區興嘉國民小學',
+  SCHOOL_NAME_EN: 'Sing Chia Elementary School',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzatki4gwED6S7IFQwsY_ctuXAP4Q5zTmRfXOrcg1EWfYYv518ZJ4ydeDJS3WoWVzfnqg/exec',
+  DEFAULT_SCHOOL_YEAR: 115
+};
